1f7c89821d20476bc975138ce04474a2d72a6b6e
Jose Cerqueira <jose@origensmedia.com>
2020-03-16 16:29:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CheckMark from "./check-mark";
 
 function App() {
+  const divRef = React.useRef();
+  const [loading, setLoading] = React.useState(false);
+  const [value, setValue] = React.useState(0);
+  const [success, setSuccess] = React.useState(false);
+  const [final, setFinal] = React.useState(false);
+
+  React.useEffect(() => {
+    const progressInterval = setInterval(() => {
+      setValue(prevState => {
+        if (prevState > 360) {
+          return 0
+        }
+
+        return prevState + 5;
+      })
+    }, 100);
+    if (success) {
+      clearInterval(progressInterval)
+    }
+    return () => {
+      clearInterval(progressInterval);
+    }
+  }, [success])
+
+  const onProgress = value => {
+    if (divRef.current)
+    divRef.current.style.setProperty('--percentage', `${value}deg`);
+  }
+
+  onProgress(value);
+
+  const handleOnSubmit = () => {
+    setLoading(true);
+    setTimeout(() =>{
+      setValue(0);
+    }, 1000);
+    setTimeout(() => {
+      setSuccess(true)
+    }, 3000);
+    setTimeout(() => {
+      setFinal(true);
+    }, 5000);
+  };
+
+  let buttonStyles = '';
+  let circularProgressStyles = '';
+
+  if (loading) {
+    buttonStyles = 'loading';
+    circularProgressStyles = 'show';
+  }
+
+
+  if (success) {
+    buttonStyles = 'success';
+    circularProgressStyles = '';
+  }
+
+  if (final) {
+    buttonStyles = 'final';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="progress-button" className={`progress-button ${buttonStyles}`}>
+        <button onClick={handleOnSubmit} disabled={loading} >
+          <span>
+            {success ? 'Message Sent' : 'Submit'}
+        </span>
+            <CheckMark className={success ? 'success svg-wrapper' : 'svg-wrapper'}/>
+        </button>
+        <div ref={divRef} className={`circular-progress ${circularProgressStyles}`}/>
+      </div>
     </div>
   );
 }
---
+++ src/check-mark.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CheckMark(props) {
+  return (
+    <svg width={22} height={18} fill="none" {...props}>
+      <path
+        d="M21.678 1.244a1.1 1.1 0 00-1.556 0L6.944 14.423 1.878 9.357a1.1 1.1 0 00-1.556 1.556l5.844 5.843a1.1 1.1 0 001.555 0L21.678 2.8a1.1 1.1 0 000-1.556z"
+        fill="#7655E9"
+      />
+    </svg>
+  );
+}
+
+export default CheckMark;
